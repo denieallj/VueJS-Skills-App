3d628e8e451b74d7285078554a4506644d6c5dca
denieallj <denieall.joe@gmail.com>
2018-12-21 18:35:19 +0800

    Complete Skills App with Vue 2

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>This is an about page for {{name}}</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna ali...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: this.$route.params.name
+    }
+  }
+}
+</script>
